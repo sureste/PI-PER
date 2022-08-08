2204545bafe981cf8956de7c3be741385160b26f
sureste <edipean@hotmail.com>
2022-08-08 01:47:14 -0500

    Created client

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -6,3 +6,14 @@ const {Tabla} = require('../db')
 
 const axios = require('axios')
 const router = Router()
+
+
+
+
+router.get('/food', async(req,res) => {
+    console.log("exito a medias")
+    res.status(200).send("estamos bien")
+})
+
+
+module.exports = router;
